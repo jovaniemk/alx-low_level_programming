b28fadcd38ca400cf99914668532a897714727b0
yoseph melaku <jovaniemk@gmail.com>
2023-03-17 13:51:15 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	int n:
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -19,7 +19,7 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
