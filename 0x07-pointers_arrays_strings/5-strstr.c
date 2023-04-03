ed98e6d6d6d06c6db2d88646648ae33d14b87219
yoseph melaku <jovaniemk@gmail.com>
2023-04-03 15:32:01 -0700

    string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strstr - locates a substring within a string
  * @haystack: string to search
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_chessboard - Entry point
- * @a: array
- * Return: Always 0 (Success)
+ * prints_chessboard - prints chess board
+ * @a: pre defined chess board
  */
-void print_chessboard(char (*a)[8])
+void print_chessboard(char (*a)[8]);
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
+		{
+			printf("%c", a[i][j])
+		}
+		printf("\n");
 	}
 }
