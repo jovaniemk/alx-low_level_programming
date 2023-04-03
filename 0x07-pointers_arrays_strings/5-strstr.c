6217344550fb0570c55bff28868bb70ef8157444
yoseph melaku <jovaniemk@gmail.com>
2023-04-03 14:49:42 -0700

    5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring within a string
+ * @haystack: string to search
+ * @needle: substring to find
+ *
+ * Return: pointers to the beginning of the substring
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*1 == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
