77f5ebcb9782aa54b35584aa12ca22baf7e46fd7
yoseph melaku <jovaniemk@gmail.com>
2023-04-03 14:01:10 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _stchr - Entry point
+ * @s: input
+ * @c: iput
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s. char c)
+{
+	int i = 0;
+
+	for (; s[s] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
