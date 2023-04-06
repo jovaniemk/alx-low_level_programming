31aed350864230346cbe8ea3c7b91669522593c4
yoseph melaku <jovaniemk@gmail.com>
2023-04-05 23:33:23 -0700

    7-print

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,7 +8,7 @@ void print_chessboard(char (*a)[8]);
 {
 	int i;
 	int j;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 	for (j = 0; j < 8; j++)
@@ -16,3 +16,4 @@ void print_chessboard(char (*a)[8]);
 	_putchar('\n');
 	}
 }
+
