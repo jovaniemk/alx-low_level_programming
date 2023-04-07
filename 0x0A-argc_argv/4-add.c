a978369ce209641f5ed7b39f21815b47d203338d
yoseph melaku <jovaniemk@gmail.com>
2023-04-07 13:42:03 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -35,7 +35,6 @@ int check_num(char *str)
  * Return: Always 0 (Success)
  */
 
-
 int main(int argc, char *argv[])
 
 {
@@ -50,15 +49,15 @@ int main(int argc, char *argv[])
 		if (check_num(argv[count]))
 		{
 
-			str_to_int = atoi(argv[count]; /*ATOI --> convert string to int*/
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
 		}
 
 		/*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
-		printf("Error\n");
-		return (1);
+			printf("Error\n");
+			return (1);
 		}
 
 		count++;
