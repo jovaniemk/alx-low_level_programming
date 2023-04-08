094dee9dc039db82669cd53d3327baefb8d4a66b
yoseph melaku <jovaniemk@gmail.com>
2023-04-07 19:40:30 -0700

    0x09-static_libraries

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -20,9 +20,9 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 	j = 0;
-	while (j < n && src [j] != '\0')
+	while (j < n && src[j] != '\0')
 	{
-		dest[i] =src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * char *_strcpy - a function that copies the string pointed by src
+ * _strcpy - a function that copies the string pointed by src
  * @dest: copy to
  * @src: copy from
  * @Return: string
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src);
 {
 	int l = 0;
 	int x = 0;
