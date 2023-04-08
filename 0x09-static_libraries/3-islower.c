93001e031801475a7de8643ad3e78d04bf073bb2
yoseph melaku <jovaniemk@gmail.com>
2023-04-07 18:32:02 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * return: i if c is lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
