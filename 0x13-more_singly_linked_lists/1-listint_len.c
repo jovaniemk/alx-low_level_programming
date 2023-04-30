acad8055ffe4256a39c08afb75125b1ac7fef556
yoseph melaku <jovaniemk@gmail.com>
2023-04-30 00:23:37 -0700

    a function that prints all elements of listint_t list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listint_len returns the number of elements in a linked lists
+ * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listsint_t to traverse
  *
  * Return: number of nodes
