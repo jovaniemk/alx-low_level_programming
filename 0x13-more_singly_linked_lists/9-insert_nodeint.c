282f2ef9eb1e421bceff8e8488dce6d47dcc1bb7
yoseph melaku <jovaniemk@gmail.com>
2023-05-02 03:09:13 -0700

    a function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,55 +1,44 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: pointer to a pointer to the head of the list
- * @n: value to store in the new node
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
  *
- * Return: address of the new element, or NULL if it failed
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new = malloc(sizeof(listint_t));
-
-	if (!new)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-
-	return (new);
-}
-/**
- * insert_nodeint_at_index - inserts a new node at a given position in a
- * listint_t linked list
- * @head: pointer to the head of the list
- * @idx: index where the new node should be added, starting from 0
- * @n: value to be stored in the new node
- *
- * Return: address of the new node, or NULL if it failed
+ * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int j;
-	listint_t *new, *pre, *now;
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
-		return (add_nodeint(head, n));
-	pre = NULL;
-	now = *head;
-	for (j = 0; now && j < idx; j++)
 	{
-		pre = now;
-		now = now->next;
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	if (j < idx)
-		return (NULL);
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (NULL);
-	new->n = n;
-	new->next = now;
-	pre->next = new;
-	return (new);
+
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+	return (NULL);
 }
