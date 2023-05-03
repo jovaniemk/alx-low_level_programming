5886600cbe8e80d48877f02c261173e33bfdc54d
yoseph melaku <jovaniemk@gmail.com>
2023-05-02 18:15:35 -0700

    a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,30 +3,31 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list
- * @head: Pointer to the head node of the list
+ * print_listint_safe - Prints a listint_t linked list.
+ * @head: Pointer to the head node of the list.
  *
- * Return: Number of nodes in the list
+ * Return: Number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *list;
-	size_t c = 0;
+        const listint_t *list;
+        size_t c = 0;
 
-	if (head == NULL)
-		exit(98);
-	while (head != NULL)
-	{
-		printf("[%p} %d\n", (void *)head, head->n);
-		c++;
-		list = head;
-		head = head->next;
+        if (head == NULL)
+                return (0);
 
-		if (list <= head)
-		{
-			printf("->[%p] %d\n", (void *)head, head->n);
-			exit(98);
-		}
-	}
-	return (c);
+        while (head != NULL)
+        {
+                printf("[%p] %d\n", (void *)head, head->n);
+                c++;
+                list = head;
+                head = head->next;
+
+                if (list >= head)
+                {
+                        printf("Loop detected, exiting...\n");
+                        exit(1);
+                }
+        }
+        return (c);
 }
