ee1c4a05d2aac77c783c92b87e3f73688840d430
jovaniemk <jovaniemk@gmail.com>
2023-07-07 12:22:44 -0700

    bit manuplated

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,6 +10,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	*n |= (lul << index);
+	*n |= (1ul << index);
 	return (1);
 }
