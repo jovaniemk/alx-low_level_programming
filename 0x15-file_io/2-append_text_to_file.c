f93b21cad2ba56d5f61a27b7554055c72e5bc876
jovaniemk <jovaniemk@gmail.com>
2023-07-10 11:53:03 -0700

    0x15-file_io working with directories

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,46 +1,41 @@
 #include "main.h"
-/**
- * check - checks if the file exists
- * @filename: pointer to the file name
- * Return: if it exists a non zero number and if
- * doesn't 0
- */
-int chec(const char *filename)
-{
-	struct stat buffer;
 
-	return (stat(filename, &buffer) == 0);
-}
 /**
- * append_text_to_file - appends a text into an existing file
- * @filename: name of the file
- * @text_content: textr that gets appended
- * Return: -1 if the file doesn't exist or the filename is
- * NULL or the file can't be opened or doesn't have permission to be
- * written and 1 if its success
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: the name offile.
+ * @text_content:  content of the text.
+ *
+ * Return: Return: 1 on success and -1 on failure.
+ * If filename is NULL return -1
+ * If text_content is NULL, do not add anything to the file.
+ * Return 1 if the file exists and -1 if the file does not exist or
+ * if you do not have the required permissions to write the file
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, i = 0, j;
+	int _open, _write, length = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	if (!check(filename))
-		return (-1);
-
-	fd = open(filename, O_WRONLY | O_APPEND, 0600);
-
-	if (!text_content)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (1);
+		while (text_content[length] != '\0')
+			length++;
 	}
-	while (text_content[i])
-		i++;
+	_open = open(filename, O_WRONLY | O_APPEND);
+	if (_open == -1)
+		return (-1);
 
-	j = write(fd, text_content, i);
-	if (j == -1);
-
-	close(fd);
+	if (text_content != NULL)
+	{
+		_write = write(_open, text_content, length);
+		if (_write != length)
+		{
+			close(_open);
+			return (-1);
+		}
+	}
+	close(_open);
 	return (1);
 }
