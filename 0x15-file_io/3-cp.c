ace259e755c7613b250412b4ccbc8e36e0d606f4
jovaniemk <jovaniemk@gmail.com>
2023-11-27 08:53:41 -0800

    C source file with 0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,81 +1,116 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void _close(char **BUF, int *fd1, int *fd2);
+/* function prototypes */
+char *create_buffer(char *file);
+void close_file(int fd);
+
 /**
- * main - main function that copies the content of
- * a file into another
- * @argc: argument counter
- * @argv: array of arguments
- * Return: 0 on success
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: Ifthe argument count is incorrect - exit code 97.
+ * if file_from does not exist or cannot be read - exit code 98.
+ * if file_to cannot be created or written to - exit code 99.
+ * if file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, *fd1 = &fd_from, *fd2 = &fd_to;
-	int j, i, n = 0;
-	char *buf, **BUF = &buf;
+	int from, to, r, wt;
+	char *buffer;
 
+	/* check for correct number of arguments */
 	if (argc != 3)
 	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	buf = malloc(sizeof(char) * 1024);
-	if (!buf)
-		return (-1);
+
+	/* Allocate 1024 bytes for buffer */
+	buffer = create_buffer(argv[2]);
+
+	/*open file_from for reading */
+	from = open(argv[1], O_RDONLY);
+
+	/* Read from file_from to buffer */
+	r = read(from, buffer, 1024);
+
+	/* creat file_to for writing */
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		i = read(fd_from, buf, 1024);
-		if (i == -1)
+		/* check if file_from or buffer can be read */
+		if (from == -1 || r == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+			free(buffer);
 			exit(98);
 		}
-		j = write(fd_to, buf, i);
-		if (j != i)
+
+		/* write from buffer to file_to */
+		wt = write(to, buffer, r);
+
+		/* check if file_to or buffer can be written */
+		if (to == -1 || wt == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+			free(buffer);
 			exit(99);
 		}
-		n++;
-	} while (i == 1024);
-	_close(BUF, fd1, fd2);
+
+		/* Read from file_from to buffer */
+		r = read(from, buffer, 1024);
+
+		/*open file_to for appending */
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	/* Free memory allocated for buffer and close files */
+	free(buffer);
+	close_file(from);
+	close_file(to);
 
 	return (0);
-}
-/**
- * _close - closes opened files and frees malloc
- * @BUF: pointer to a buffer
- * @fd1: pointer to the first file
- * @fd2: pointer to the second file
- *
- */
-void _close(char **BUF, int *fd1, int *fd2)
-{
-	int n, m;
-
-	free(*BUF);
-	n = close(*fd1);
-	if (n == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", *fd1);
-		exit(100);
-	}
-	m = close(*fd2);
-	if (m == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", *fd2);
-		exit(100);
-	}
 }
